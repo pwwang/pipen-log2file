65c8a5e0b42394d1cd138b8aa65fd2fbfa27a8b4
pwwang <pwwang@pwwang.com>
2023-04-13 16:57:20 -0700

    🔖 0.2.0

--- pipen_log2file.py
+++ pipen_log2file.py
@@ -14,7 +14,7 @@ if TYPE_CHECKING:  # pragma: no cover
     from pipen import Pipen, Proc
     from pipen.job import Job
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 xqute_logger_handlers = xqute_logger.handlers
 
