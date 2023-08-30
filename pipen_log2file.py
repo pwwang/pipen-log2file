b8a9ecd570fa50fe310b46ff370b98e7e2f061f2
pwwang <1188067+pwwang@users.noreply.github.com>
2023-08-30 16:48:20 -0700

    0.3.4

--- pipen_log2file.py
+++ pipen_log2file.py
@@ -15,7 +15,7 @@ if TYPE_CHECKING:  # pragma: no cover
     from pipen import Pipen, Proc
     from pipen.job import Job
 
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 
 xqute_logger_handlers = xqute_logger.handlers
 
