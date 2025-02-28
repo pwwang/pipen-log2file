454da9b6721c128523371aa055945ada8c950a3c
pwwang <pwwang@pwwang.com>
2025-02-28 12:54:59 -0700

    feat: add example scripts for cloud processing

---
+++ examples/example-cloud.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+from pipen import Proc, Pipen
+
+load_dotenv()
+BUCKET = "gs://handy-buffer-287000.appspot.com"
+
+
+class ExampleProc(Proc):
+    input = "in"
+    output = "out:var:{{in.in}}"
+    script = "echo 1"
+
+
+class ExamplePipelineCloudWdir(Pipen):
+    starts = ExampleProc
+    # cache = False
+    data = [["a", "b", "c", "d", "e"]]
+    workdir = f"{BUCKET}/pipen-log2file-example"
+    loglevel = "debug"
+
+
+if __name__ == "__main__":
+    ExamplePipelineCloudWdir(plugins=["log2file"]).run()
---
+++ examples/example.py
@@ -0,0 +1,18 @@
+from pipen import Pipen, Proc
+
+
+class AProc(Proc):
+    input = "a,b:file"
+    output = "b:file:{{in.a}}.txt"
+    input_data = [(1, __file__), (2, __file__)]
+    envs = {
+        "x": "[a]\nb=1\n",
+        "xyz": "[a]\nb=1\n",
+    }
+    script = "echo 123 > {{out.b}}"
+
+
+pipen = Pipen(loglevel="debug").set_start(AProc)
+
+if __name__ == "__main__":
+    pipen.run()
