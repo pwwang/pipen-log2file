f4887c5ebe01c720901d728bd8cbf54e4b39db0e
pwwang <pwwang@pwwang.com>
2025-03-07 20:46:09 -0700

    chore: bump version to 0.10.0

--- pipen_log2file.py
+++ pipen_log2file.py
@@ -21,7 +21,7 @@ if TYPE_CHECKING:  # pragma: no cover
     from pipen import Pipen, Proc
     from pipen.job import Job
 
-__version__ = "0.9.0"
+__version__ = "0.10.0"
 
 xqute_logger_handlers = xqute_logger.handlers
 
